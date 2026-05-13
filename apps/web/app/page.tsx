8801e3ab0fb612196317b8cf843de505116b4c21
Brian Love <brian@liveloveapp.com>
2026-05-12 20:36:43 -0700

    feat(web): wire KeepTheRuntime + Ecosystem + Quickstart; drop 6 cosmic sections

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,18 @@
-import { ArchitectureSection } from "./components/landing/ArchitectureSection"
-import { BigReveal } from "./components/landing/BigReveal"
-import { ComicStrip } from "./components/landing/ComicStrip"
-import { ComparisonTable } from "./components/landing/ComparisonTable"
-import { CtaSection } from "./components/landing/CtaSection"
-import { DeploySection } from "./components/landing/DeploySection"
-import { EcosystemSection } from "./components/landing/EcosystemSection"
-import { HowItWorks } from "./components/landing/HowItWorks"
-import { MigrateCta } from "./components/landing/MigrateCta"
-import { NotAReplacement } from "./components/landing/NotAReplacement"
-import { StarsSection } from "./components/landing/StarsSection"
-import { FeatureDevLoop } from "./components/landing-v2/FeatureDevLoop"
-import { FeatureRouting } from "./components/landing-v2/FeatureRouting"
-import { FeatureTools } from "./components/landing-v2/FeatureTools"
-import { FeatureTypes } from "./components/landing-v2/FeatureTypes"
 import { Hero } from "./components/landing-v2/Hero"
 import { ProofStrip } from "./components/landing-v2/ProofStrip"
 import { WhyDawn } from "./components/landing-v2/WhyDawn"
+import { FeatureRouting } from "./components/landing-v2/FeatureRouting"
+import { FeatureTools } from "./components/landing-v2/FeatureTools"
+import { FeatureTypes } from "./components/landing-v2/FeatureTypes"
+import { FeatureDevLoop } from "./components/landing-v2/FeatureDevLoop"
+import { KeepTheRuntime } from "./components/landing-v2/KeepTheRuntime"
+import { Ecosystem } from "./components/landing-v2/Ecosystem"
+import { Quickstart } from "./components/landing-v2/Quickstart"
+import { BigReveal } from "./components/landing/BigReveal"
+import { ComicStrip } from "./components/landing/ComicStrip"
+import { CtaSection } from "./components/landing/CtaSection"
+import { MigrateCta } from "./components/landing/MigrateCta"
+import { StarsSection } from "./components/landing/StarsSection"
 import { PaletteScroller } from "./components/PaletteScroller"
 import { ScrollReveal } from "./components/ScrollReveal"
 
@@ -29,34 +26,19 @@ export default function HomePage() {
       <FeatureTools />
       <FeatureTypes />
       <FeatureDevLoop />
+      <KeepTheRuntime />
+      <Ecosystem />
+      <Quickstart />
       <div className="landing-dark relative isolate">
         <PaletteScroller />
         <ScrollReveal>
           <ComicStrip />
         </ScrollReveal>
         <ScrollReveal>
-          <ComparisonTable />
-        </ScrollReveal>
-        <ScrollReveal>
           <BigReveal />
         </ScrollReveal>
         <ScrollReveal>
-          <ArchitectureSection />
-        </ScrollReveal>
-        <ScrollReveal>
-          <DeploySection />
-        </ScrollReveal>
-        <ScrollReveal>
-          <EcosystemSection />
-        </ScrollReveal>
-        <ScrollReveal>
-          <HowItWorks />
-        </ScrollReveal>
-        <ScrollReveal>
           <StarsSection />
-        </ScrollReveal>
-        <ScrollReveal>
-          <NotAReplacement />
         </ScrollReveal>
         <ScrollReveal>
           <MigrateCta />
