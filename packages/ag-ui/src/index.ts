01dae2de83be987bb42f7c8a0c29a91703bd3b11
Brian Love <brian@liveloveapp.com>
2026-07-08 20:22:29 -0700

    feat(ag-ui): public barrel + interrupt round-trip test

--- packages/ag-ui/src/index.ts
+++ packages/ag-ui/src/index.ts
@@ -1,4 +1,23 @@
 export { encodeAgUiSse } from "./encode.js"
+export { createCounterIdFactory, createDefaultIdFactory, type IdFactory } from "./ids.js"
+export { type DawnMessage, type DawnRunInput, fromRunAgentInput } from "./inbound.js"
+export {
+  type DawnInterruptEnvelope,
+  type DawnResumeRequest,
+  fromAguiResume,
+  toAguiInterrupt,
+} from "./interrupts.js"
+export { type AguiOutboundEvent, type ToAguiOptions, toAguiEvents } from "./outbound.js"
 export { type MappedRunInput, mapRunInput, type ResumeDecision } from "./run-input.js"
 export { type AgUiTranslator, createAgUiTranslator } from "./translate.js"
-export type { AgUiEvent, DawnStreamChunk, TranslatorOptions } from "./types.js"
+export {
+  asToolCallData,
+  asToolResultData,
+  type AgUiEvent,
+  type DawnStreamChunk,
+  type DawnToolCallData,
+  type DawnToolResultData,
+  type RawChunk,
+  type RunContext,
+  type TranslatorOptions,
+} from "./types.js"
